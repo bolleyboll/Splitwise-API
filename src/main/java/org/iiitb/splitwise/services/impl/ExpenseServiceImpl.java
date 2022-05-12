0678f79b8f0cd778ffb6fe5c78fa6bec426b5f0d
Aman Gupta <aman.iv0012@gmail.com>
2022-05-12 10:51:12 +0530

    Expense Done

--- src/main/java/org/iiitb/splitwise/services/impl/ExpenseServiceImpl.java
+++ src/main/java/org/iiitb/splitwise/services/impl/ExpenseServiceImpl.java
@@ -1,7 +1,9 @@
 package org.iiitb.splitwise.services.impl;
 
 import org.iiitb.splitwise.model.Expense;
+import org.iiitb.splitwise.model.Group;
 import org.iiitb.splitwise.repositories.ExpenseRepository;
+import org.iiitb.splitwise.repositories.GroupRepository;
 import org.iiitb.splitwise.services.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,14 +11,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class ExpenseServiceImpl implements ExpenseService{
+public class ExpenseServiceImpl implements ExpenseService {
+
+	@Autowired
+	private GroupRepository gr;
 
 	@Autowired
 	private ExpenseRepository er;
-	
+
 	@Override
 	public Expense add(Expense expense) {
-		return er.save(expense);
+		String paidBy = expense.getSplitBetween();
+		Double amt = expense.getAmount();
+		String f = paidBy + ": +" + amt.toString() + ", ";
+		Integer grpId = Integer.parseInt(expense.getForGroup());
+		Group g = gr.findByGroupId(grpId);
+		String members = g.getMembers();
+		String memL[] = members.split(", ");
+		int count = memL.length - 1;
+		Double split = amt / count;
+		for (String member : memL) {
+			if (member.equalsIgnoreCase(paidBy)) {
+				continue;
+			} else {
+				f += member + ": -" + split.toString() + ", ";
+			}
+		}
+		expense.setSplitBetween(f);
+		Expense e = er.save(expense);
+		return e;
 	}
-	
+
 }
