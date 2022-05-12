102988c41765070665492b498f81b50ff39ae841
Aman Gupta <aman.iv0012@gmail.com>
2022-05-12 19:26:05 +0530

    logback

--- src/main/java/org/iiitb/splitwise/SplitwiseApplication.java
+++ src/main/java/org/iiitb/splitwise/SplitwiseApplication.java
@@ -2,12 +2,13 @@ package org.iiitb.splitwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @SpringBootApplication
 public class SplitwiseApplication {
+	private static final Logger log = LogManager.getLogger(SplitwiseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SplitwiseApplication.class, args);
